2ab56941a94a0a47a3b64cf8b4e9801384f23840
PatriciaR0 <reperger.patricia@gmail.com>
2025-02-13 20:06:12 +0100

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,44 @@
+<script>
+    import { onMount } from "svelte";
+    let logoSrc = "/logo.png"; // A static mappában van
+</script>
+
+<svelte:head>
+    <link rel="icon" type="image/png" href="/logo.png" />
+</svelte:head>
+
+<main>
+    <img class="logo" src={logoSrc} alt="AstroWheel Logo" />
+    <slot />
+</main>
+
+<footer class="footer">Made by: Witches Brew Games 2025</footer>
+
+<style>
+    main {
+        height: 100%;
+        margin: 0;
+        padding: 0;
+        background: url("/background.png") no-repeat center center fixed;
+        background-size: cover;
+        font-family: Arial, sans-serif;
+        color: white;
+        text-align: center;
+    }
+
+    .logo {
+        width: 370px; 
+        height: auto;
+        margin-top: 80px;
+    }
+
+    .footer {
+        position: absolute;
+        bottom: 0;
+        width: 100%;
+        background-color: black;
+        padding: 10px 0;
+        color: white;
+        text-align: center;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    let title = "Üdvözlünk az AstroWheel világában!";
+</script>
+
+<style>
+    .container {
+        margin-top: 20px;
+        font-size: 24px;
+    }
+</style>
+
+<div class="container">
+    <h1>{title}</h1>
+</div>
