d589931fc98da21a74621aa3a1df015138ba6737
PatriciaR0 <reperger.patricia@gmail.com>
2025-03-13 21:39:03 +0100

    Characters page base

--- src/routes/home/characters/+layout.svelte
+++ src/routes/home/characters/+layout.svelte
@@ -1,13 +1,66 @@
+<script>
+    let message = "";
+    let character_info = {};
+
+    async function add() {
+        const response = await fetch("http://localhost:3000/api/Inventory", {
+            method: "GET",
+            headers: {
+                "content-type": "application/json",
+            },
+        });
+
+        character_info = await response.json();
+    }
+
+    // Képek adatai
+    const images = [
+        { src: "/npc Sagittarius.png", alt: "Sagittarius" },
+        { src: "/npc Capricorn.png", alt: "Capricorn" },
+        { src: "/npc Aquarius.png", alt: "Aquarius" },
+        { src: "/npc Pisces.png", alt: "Pisces" },
+        { src: "/npc Aries.png", alt: "Aries" },
+        { src: "/npc Taurus.png", alt: "Taurus" },
+        { src: "/npc Gemini.png", alt: "Gemini" },
+        { src: "/npc Cancer.png", alt: "Cancer" },
+        { src: "/npc Leo.png", alt: "Leo" },
+        { src: "/npc Virgo.png", alt: "Virgo" },
+        { src: "/npc Libra.png", alt: "Libra" },
+        { src: "/npc Scorpio.png", alt: "Scorpio" },
+    ];
+</script>
+
+{character_info.level}
+{character_info.last_visited_island}
+
+<button onclick={add}>Get character info</button>
+
 <div class="home-container">
     <div class="content">
         <h1>CHARACTERS</h1>
     </div>
 </div>
 
+
+<!-- Felső sor -->
+<div class="grid grid-cols-6 gap-4 mb-4">
+    {#each images.slice(0, 6) as image}
+        <img src={image.src} alt={image.alt} class="npc_pic" />
+    {/each}
+</div>
+
+<!-- Alsó sor -->
+<div class="grid grid-cols-6 gap-4">
+    {#each images.slice(6) as image}
+        <img src={image.src} alt={image.alt} class="npc_pic" />
+    {/each}
+</div>
+
 <style>
     .home-container {
         margin-left: 300px;
         display: flex;
+        align-items: center;
     }
 
     .content {
@@ -24,4 +77,15 @@
         align-items: right;
         font-size: 24px;
     }
+
+    .npc_pic {
+    height: 200px;
+    width: 200px;
+    object-fit: cover;
+    padding: 5px;
+    padding-top: 50px;
+    padding-bottom:  50px;
+    margin-top: 20px;
+    margin-left: 20px;
+  }
 </style>
